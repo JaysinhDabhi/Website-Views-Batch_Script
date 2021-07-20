f66631c5bbf9d08f97d6f08e37fe3468d93cc5ca
Jaysinh Dabhi <87559228+JaysinhDabhi@users.noreply.github.com>
2021-07-20 09:38:03 +0530

    Add files via upload

---
+++ WEBSITE VIEWS.bat
@@ -0,0 +1,21 @@
+
+@ECHO OFF
+
+:LOOP
+
+
+
+
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+start /d "c:\Program files\Google Chrome" Chrome.exe --incognito "Your Website URL With Full Link Here Example:-https://adaptedtech.blogspot.com/ "
+
+
+GOTO LOOP
+
+EXIT
